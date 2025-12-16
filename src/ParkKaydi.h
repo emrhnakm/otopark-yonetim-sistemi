0542f64be9dca450813e25c411e0b15c94d4166e
Muharrem Emirhan AKMA <makma1@ogr.iu.edu.tr>
2025-12-16 20:01:11 +0300

    Update ParkKaydi.h

--- src/ParkKaydi.h
+++ src/ParkKaydi.h
@@ -1,1 +1,32 @@
-class ParkKaydi {};
+#ifndef PARKKAYDI_H
+#define PARKKAYDI_H
+
+#include "Arac.h"     
+#include "ParkYeri.h"  
+
+class ParkKaydi {
+private:
+    Arac* arac;        // Arac nesnesi
+    ParkYeri* parkYeri;
+    int girisSaati;
+    int cikisSaati;
+    double ucret;
+
+public:
+    // Kayıt: Girişte oluşur
+    ParkKaydi(Arac* a, ParkYeri* p, int giris);
+
+    ~ParkKaydi();
+
+    // İşlemler
+    void cikisYap(int cikis); // Çıkış saatini alır, ücreti hesaplar
+    
+    // Bilgi Getirme 
+    Arac* getArac() const;
+    ParkYeri* getParkYeri() const;
+    double getUcret() const;
+    int getGirisSaati() const;
+    int getCikisSaati() const;
+};
+
+#endif
