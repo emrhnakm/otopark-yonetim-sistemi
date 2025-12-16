92b6fee762ae5ed7c936c5dd1cb4dbba30445637
Muharrem Emirhan AKMA <makma1@ogr.iu.edu.tr>
2025-12-16 20:15:00 +0300

    Create Otopark.h

---
+++ src/Otopark.h
@@ -0,0 +1,34 @@
+#ifndef OTOPARK_H
+#define OTOPARK_H
+
+#include <vector>
+#include <string>
+
+#include "ParkKaydi.h"
+#include "ParkYeri.h"
+
+using namespace std;
+
+class Otopark {
+private:
+    vector<ParkKaydi> aktifKayitlar;   // Otoparktaki aktif araçlar
+    vector<ParkYeri> parkYerleri;      // Otoparktaki park yerleri
+
+public:
+    // Park yeri ekleme
+    void parkYeriEkle(const ParkYeri& p);
+
+    // Araç giriş
+    void aracEkle(const ParkKaydi& kayit);
+
+    // Plakaya göre arama
+    ParkKaydi* plakaAra(const string& plaka);
+
+    // Araç çıkışı
+    void aracCikar(const string& plaka, int cikisSaati);
+
+    // Toplam gelir
+    double toplamGelir() const;
+};
+
+#endif
