c5cb5976d08b9ee315e1655fb20a7429ccb3cf23
Muharrem Emirhan AKMA <makma1@ogr.iu.edu.tr>
2025-12-16 20:11:43 +0300

    Refactor comments in ParkKaydi.cpp
    Updated comments for clarity and consistency.

---
+++ src/ParkKaydi.cpp
@@ -0,0 +1,42 @@
+#include "ParkKaydi.h"
+
+// Girişte kayıt oluşturur.
+ParkKaydi::ParkKaydi(Arac* a, ParkYeri* p, int giris)
+    : arac(a), parkYeri(p), girisSaati(giris), cikisSaati(-1), ucret(0.0)
+{
+}
+
+ParkKaydi::~ParkKaydi() {
+}
+
+// Araç çıkışı
+void ParkKaydi::cikisYap(int cikis) {
+    cikisSaati = cikis;
+    int sure = cikisSaati - girisSaati;
+
+    if (sure < 0)
+        sure = 0;
+
+    // Ücret hesaplama
+    ucret = arac->hesaplaUcret(sure);
+}
+
+Arac* ParkKaydi::getArac() const {
+    return arac;
+}
+
+ParkYeri* ParkKaydi::getParkYeri() const {
+    return parkYeri;
+}
+
+double ParkKaydi::getUcret() const {
+    return ucret;
+}
+
+int ParkKaydi::getGirisSaati() const {
+    return girisSaati;
+}
+
+int ParkKaydi::getCikisSaati() const {
+    return cikisSaati;
+}
