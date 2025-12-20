6be0d2b9d76ebae54a4bb0efac9f67787f42a6d3
silaata <silaata175@gmail.com>
2025-12-20 23:40:14 +0300

    Update Arac.h

--- src/Arac.h
+++ src/Arac.h
@@ -3,37 +3,31 @@
 
 #include <string>
 
-// Temel Araç Sınıfı
 class Arac {
 protected:
     std::string plaka;
 public:
-    Arac(const std::string& plaka);
+    Arac(const std::string& plaka); 
     virtual ~Arac();
 
     const std::string& getPlaka() const;
 
-    // Polimorfizm kullanılan fonksiyonlar
     virtual double hesaplaUcret(int sure) const = 0;
-    virtual std::string getTur() const = 0; // = 0 ile temel sınıftan nesne oluşumu engellenir
+    virtual std::string getTur() const = 0;
 };
 
-//Türemiş sınıf(Otomobil)
 class Otomobil : public Arac {
-    public:
-        Otomobil(const std::string& plaka);
-        
-        double hesaplaUcret(int sure) const override;
-        std::string getTur() const override;
+public:
+    Otomobil(const std::string& plaka);
+    double hesaplaUcret(int sure) const override;
+    std::string getTur() const override;
 };
 
-//Türemiş sınıf (Kamyonet)
 class Kamyonet : public Arac {
-    public:
-        Kamyonet(const std::string& plaka);
-        
-        double hesaplaUcret(int sure) const override;
-        std::string getTur() const override;
+public:
+    Kamyonet(const std::string& plaka);
+    double hesaplaUcret(int sure) const override;
+    std::string getTur() const override;
 };
 
 #endif
