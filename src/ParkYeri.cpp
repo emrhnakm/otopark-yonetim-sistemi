ff1b75dcc4887a4e00135ac644f8526c2d09e820
Dr-Mz42 <fizikodev4132@gmail.com>
2025-12-20 18:55:06 +0300

    Update ParkYeri.cpp

--- src/ParkYeri.cpp
+++ src/ParkYeri.cpp
@@ -1,1 +1,21 @@
+#include "ParkYeri.h"
+
+ParkYeri::ParkYeri(int _id, std::string _konum)
+    : id(_id), konum(_konum), doluMu(false) {}
+
+int ParkYeri::getId() const {}
+    return id;
+}
+
+std::string ParkYeri::getKonum() const {
+    return konum;
+}
+
+bool ParkYeri::isDolu() const {
+    return doluMu;
+}
+
+void ParkYeri::setDolu(bool durum) {
+    doluMu = durum;
+}
 
